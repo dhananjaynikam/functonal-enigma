d25010e17381cd7a899daf87bba82bdf0b5218c9
Dhananjay <dhananjay.nikam711@gmail.com>
2018-02-07 09:49:10 +0530

    Start1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("lets do this");
-        System.out.print("sgw");
+        System.out.print("sg");
     }
 }
