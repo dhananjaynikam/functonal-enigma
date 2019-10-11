12e4cf3781535a05462b14f526bc199b7cd1ceea
Dhananjay <dhananjay.nikam711@gmail.com>
2019-10-10 20:21:45 -0400

    LongestSubstring using the visitor method

---
+++ src/com/hackerRank/LongestSubstring.java
@@ -0,0 +1,41 @@
+package hackerRank;
+
+public class LongestSubstring {
+    public static void main(String args[]) {
+        String s = "pwwkew";
+        String a = "au";
+        String b = "dvdf";
+        System.out.println(lengthOfLongestSubstring(s));
+        System.out.println(lengthOfLongestSubstring(a));
+        System.out.println(lengthOfLongestSubstring(b));
+
+}
+    public static int lengthOfLongestSubstring(String str){
+        final int NO_OF_CHARS = 256;
+        int n = str.length();
+        int cur_len = 1;
+        int max_len = 1;
+        int prev_index;
+        int visited[] = new int[NO_OF_CHARS];
+        for (int i = 0; i < NO_OF_CHARS; i++) {
+            visited[i] = -1;
+        }
+        visited[str.charAt(0)] = 0;
+        for (int i = 1; i < n; i++) {
+            prev_index = visited[str.charAt(i)];
+            if (prev_index == -1 || i - cur_len > prev_index)
+                cur_len++;
+            else {
+                if (cur_len > max_len)
+                    max_len = cur_len;
+
+                cur_len = i - prev_index;
+            }
+            visited[str.charAt(i)] = i;
+        }
+        if (cur_len > max_len)
+            max_len = cur_len;
+
+        return max_len;
+    }
+}
