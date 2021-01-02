46b99c6eb061b3aa952564bd187fa028bed90d30
Dhananjay Nikam <54861482+dhananjaynikam@users.noreply.github.com>
2021-01-02 13:56:44 -0500

    Leetcode

--- src/com/leetCode/RemoveVowels.java
+++ src/com/leetCode/RemoveVowels.java
@@ -1,13 +1,23 @@
 package com.leetCode;
 
+import java.util.ArrayList;
+
 public class RemoveVowels {
 
     public static void main(String args[]){
-
+        String s = "leetcodeisacommunityforcoders";
+        System.out.print(removeVowels(s));
     }
 
     public static String removeVowels(String s){
-
-        return "";
+        String vowels = "aeiou";
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char t = s.charAt(i);
+            if(vowels.indexOf(t) == -1 ){
+                ans.append(t);
+            }
+        }
+        return ans.toString();
     }
 }
